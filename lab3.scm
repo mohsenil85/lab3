2100345831efb57541b5a5e4501d36df61e9a6ad
mohsenil85 <mohsenil85@gmail.com>
2013-04-05 10:57:37 -0600

    poppin molly im sweatin

--- lab3.scm
+++ lab3.scm
@@ -3,7 +3,28 @@
     '()
     (cons (+ 1 (car lst)) (f (cdr lst)))))
 
+(define (all-ints? lst)
+  (if  (memq #f (map integer? lst))
+    #f
+    #t))
+
+;returns true if there are duplicates and false if there are not
+(define (dupes? lst)
+  (cond ((null? lst) #f)
+        ((member (car lst) (cdr lst) ) #t)
+        (else (dupes? (cdr lst)))))
+
 (define (good-set? lst)
-  (if (not (memq #f (map integer? lst)))
-    #t
-    #f))
+  (and (all-ints? lst) (not (dupes? lst))))
+
+(define (member? e lst)
+  (memq #t
+    (map (lambda (x)
+           (eq? e x))
+         lst)))
+
+(define (union lst1 lst2)
+  (cond ((or (null? lst1) (null? lst2))
+         "null list!")
+        ((
+ 
